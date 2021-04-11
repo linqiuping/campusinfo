cf18ddd31017ba27e9f3381a789a990e0250f73c
linqiuping <lynn.lqp@alibaba-inc.com>
2021-04-11 17:09:31 +0800

    下载网页的接口

--- src/main/java/com/example/campusinfo/controller/DownloadingWebpage.java
+++ src/main/java/com/example/campusinfo/controller/DownloadingWebpage.java
@@ -1,24 +1,44 @@
 package com.example.campusinfo.controller;
 
+import org.springframework.stereotype.Service;
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.InputStreamReader;
 import java.net.URL;
 
+
+@Service
 public class DownloadingWebpage {
-    public static void main(String[] args) throws Exception {
-        URL url = new URL("http://zjut.jysd.com/job/search?d_category=0");
+    public  String getContentFormPage(String urlStr)throws Throwable{
+        URL url = new URL(urlStr);
         BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
         BufferedWriter writer = new BufferedWriter(new FileWriter("save2yiibai-index.html"));
+        String content="";
         String line;
-
         while ((line = reader.readLine()) != null) {
             System.out.println(line);
             writer.write(line);
             writer.newLine();
+            content+=line;
         }
         reader.close();
         writer.close();
+        return content;
     }
+//    public static void main(String[] args) throws Exception {
+//        URL url = new URL("http://zjut.jysd.com/job/search?d_category=0");
+//        BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
+//        BufferedWriter writer = new BufferedWriter(new FileWriter("save2yiibai-index.html"));
+//        String line;
+//
+//        while ((line = reader.readLine()) != null) {
+//            System.out.println(line);
+//            writer.write(line);
+//            writer.newLine();
+//        }
+//        reader.close();
+//        writer.close();
+//    }
 }
--- src/main/java/com/example/campusinfo/controller/PageController.java
+++ src/main/java/com/example/campusinfo/controller/PageController.java
@@ -1,6 +1,8 @@
 package com.example.campusinfo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -10,10 +12,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/page/")
 public class PageController {
 
-    @RequestMapping("downloadWebpage")
-    public String  downloadWebpage(){
+    @Autowired
+    DownloadingWebpage downloadingWebpage;
 
-        return "hi";
+    @RequestMapping("downloadWebpage")
+    public String  downloadWebpage(@RequestParam String url){
+        try {
+            return downloadingWebpage.getContentFormPage(url);
+        }catch (Throwable throwable){
+            throwable.printStackTrace();
+            return "url error";
+        }
+
     }
 
 }
